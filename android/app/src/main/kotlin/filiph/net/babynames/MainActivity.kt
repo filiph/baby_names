a12642c9edd37b2c85c9f64748e971e7807f2e47
Filip Hracek <filip.hracek@gmail.com>
2018-09-20 16:04:08 -0700

    Full example (without new/const)

---
+++ android/app/src/main/kotlin/filiph/net/babynames/MainActivity.kt
@@ -0,0 +1,13 @@
+package filiph.net.babynames
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
